e04d69a96aa7a3bd8f2b94d285c7742238099979
caiofloroads <caiofloroads@gmail.com>
2020-09-28 20:17:17 +0000

    Terminando de configurar o Mongoose!

--- back-end/app.js
+++ back-end/app.js
@@ -7,7 +7,11 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 const db = require('./config/database')
-//db('mongodb+srv://caiofloroads:<password>@cluster0.bx6mz.gcp.mongodb.net/db_projetoweb?retryWrites=true&w=majority')
+const dbUser = process.env.DB_USER
+const dbPass = process.env.DB_PASS
+const dbName = process.env.DB_NAME
+
+db(`mongodb+srv://${dbUser}:${dbPass}@cluster0.bx6mz.gcp.mongodb.net/${dbName}?retryWrites=true&w=majority`)
 
 var app = express();
 
--- back-end/config/database.js
+++ back-end/config/database.js
@@ -1,5 +1,23 @@
 const mongoose = require('mongoose')
 
 module.exports = uri => {
-    mongoose.connect(uri)
+    mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+
+    mongoose.connection.on('connected', () =>
+        console.log('==> Mongoose! conectado com sucesso ao servidor')
+    )
+
+    process.on('SIGINT', () =>
+        mongoose.connection.close(() => {
+            console.log('==> Mongoose! Desconectado pelo término da aplicação')
+            process.exit(0)
+        })
+    )
+
+    mongoose.connection.on('disconnected', () => 
+        console.log('==> Mongoose! desconectado do servidor')
+    )
 }
