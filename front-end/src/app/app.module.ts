857e3d78965ad40470c1bc9d971eab9a6a4137e5
caiofloroads <caiofloroads@gmail.com>
2020-12-12 09:15:48 +0000

    12/12

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,25 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module';
-import { MainToolbarComponent } from './ui/main-toolbar/main-toolbar.component';
-import { MainMenuComponent } from './ui/main-menu/main-menu.component';
-import { MainFooterComponent } from './ui/main-footer/main-footer.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainToolbarComponent,
-    MainMenuComponent,
-    MainFooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
